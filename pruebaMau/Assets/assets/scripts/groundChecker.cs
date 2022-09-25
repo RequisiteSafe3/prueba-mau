bebe9f4fa9e27a38d528ad38350701e8facd3bdf
Amongo <requisitesafe3@live.com>
2022-09-25 18:22:20 -0500

    LightsAdded

--- pruebaMau/Assets/assets/scripts/groundChecker.cs
+++ pruebaMau/Assets/assets/scripts/groundChecker.cs
@@ -7,7 +7,9 @@ public class groundChecker : MonoBehaviour
     public static bool isGrounded;
 
     private void OnTriggerEnter2D(Collider2D collision) { // CheckGround.
-        isGrounded = true;
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision) {
         isGrounded = false;
--- pruebaMau/Assets/assets/scripts/helpDots.cs
+++ pruebaMau/Assets/assets/scripts/helpDots.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class helpDots : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private bool TRange = false;
+    [SerializeField] private GameObject Object;
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Object.gameObject.SetActive(true);
+            TRange = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerExit2D(Collider2D collision) {
+        Object.gameObject.SetActive(false);
+        TRange = false;
     }
 }
